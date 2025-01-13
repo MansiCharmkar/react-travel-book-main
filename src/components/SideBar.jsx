ff5eab37921f6bf7cb916ed2265850f4c9829635
Mansi Charmkar <mansic632@gmail.com>
2025-01-13 14:23:49 +0530

    Project Initiated

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,26 @@
+import style from "../styles/sideBar.module.css";
+import LogoBox from "../components/LogoBox";
+import AppNav from "./AppNav";
+import CityList from "./CityList";
+import Footer from "./Footer";
+import City from "./City";
+import AddCityForm from "./AddCityForm";
+import CountryList from "./CountryList";
+
+function SideBar() {
+    return (
+        <div className={style.sideBar}>
+            <LogoBox />
+            <AppNav />
+
+            {/* <CityList /> */}
+            {/* <AddCityForm /> */}
+            {/* <City /> */}
+            <CountryList />
+
+            <Footer />
+        </div>
+    );
+}
+
+export default SideBar;
